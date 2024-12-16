df0996e00db305666849e88b1e5e43bb3b399206
Jayreddin <jayreddin@hotmail.com>
2024-12-16 00:16:27 +0000

    111

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function toggleGithubProjects() {
+    var githubProjectsContainer = document.getElementById('github-projects-container');
+    if (githubProjectsContainer.style.display === 'none' || githubProjectsContainer.style.display === '') {
+        githubProjectsContainer.style.display = 'block';
+    } else {
+        githubProjectsContainer.style.display = 'none';
+    }
+}
 
+function toggleAboutMe() {
+    var aboutMeContainer = document.getElementById('about-me-container');
+    if (aboutMeContainer.style.display === 'none' || aboutMeContainer.style.display === '') {
+        aboutMeContainer.style.display = 'block';
+    } else {
+        aboutMeContainer.style.display = 'none';
+    }
+}
